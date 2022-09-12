5c17267bd0e97b5e49c205d65797c81a58e61081
cyrus998 <jonathanfranciscosopro@gmail.com>
2022-09-12 11:59:35 +0800

    updated dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,88 @@
 <template>
+  <v-row no-gutters style="padding: 2rem; ">
+
+    <v-col cols="12" sm="4" style="margin-top: 1rem;">
+      <v-card  max-width="350">
+        <router-link to="/about">
+          <img :src="vuetifylink" height="200" cover class="white--text align-end" />
+        </router-link>
+        <v-card-title>About Me.</v-card-title>
+        <v-card-text class="text--primary">
+          <div>Contains some basic information about me.</div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" sm="4" style="margin-top: 1rem;">
+      <v-card  max-width="350">
+        <router-link to="/aboutvuetify">
+          <img :src="vuetifylink" height="200" cover class="white--text align-end" />
+        </router-link>
+        <v-card-title>About Vuetify.</v-card-title>
+        <v-card-text class="text--primary">
+          <div>Contains a brief introduction about Vuetify</div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+
+    <v-col cols="12" sm="4" style="margin-top: 1rem;">
+      <v-card  max-width="350">
+        <router-link to="/calculator">
+          <img :src="vuetifylink" height="200" cover class="white--text align-end" />
+        </router-link>
+        <v-card-title>Simple Calculator.</v-card-title>
+        <v-card-text class="text--primary">
+          <div>A really basic calculator.</div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" sm="4" style="margin-top: 1rem;">
+      <v-card  max-width="350">
+        <router-link to="/string">
+          <img :src="vuetifylink" height="200" cover class="white--text align-end" />
+        </router-link>
+        <v-card-title>String Manipulation.</v-card-title>
+        <v-card-text class="text--primary">
+          <div>A really basic simple calculator.</div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+    <v-col cols="12" sm="4" style="margin-top: 1rem;">
+      <v-card  max-width="350">
+        <router-link to="/quizapp">
+          <img :src="vuetifylink" height="200" cover class="white--text align-end" />
+        </router-link>
+        <v-card-title>Quiz App.</v-card-title>
+        <v-card-text class="text--primary">
+          <div>A Quiz app powered by axios, and opentdb API.</div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+
+
+    
 
 
 
+    
+  </v-row>
 
+  
 </template>
 
+
 <script>
-import { defineComponent } from 'vue';
+import image from "../assets/aboutmelink.png";
+import vuetifylink from "../assets/vuetifylink.png";
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
+export default {
+  data: function () {
+    return {
+      image: image, vuetifylink
+    };
   },
-});
+};
 </script>
